15e2ce98ed2b7ef335089e169d00f69a46f5c8c2
Juan Camilo Mamian Ruiz <mr.machineman98@gmail.com>
2024-02-04 11:15:45 -0500

    finalizacion maquetacion form

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,5 +1,34 @@
+import { useState } from "react";
+
+const initialUserForm = {
+    username: '',
+    password: '',
+    email: ''
+};
+
 export const UserForm = () => {
-    return (<>
-        <p>Formulario de usuario</p>
-    </>);
+
+    const [userForm, setUserForm] = useState(initialUserForm);
+    const {username, password, email} = userForm;
+
+    const onInputChange = ({target}) => {
+        //console.log(target.value);
+        let {name, value} = target;
+        setUserForm({
+            ...userForm,
+            [name]: value
+        });
+    };
+
+    return (<form action="">
+
+        <input onChange={ onInputChange } className="form-control my-3 w-75" placeholder="Username" name="username" value={username}/>
+
+        <input onChange={ onInputChange } type="password" className="form-control my-3 w-75" placeholder="Password" name="password" value={password}/>
+
+        <input onChange={ onInputChange } className="form-control my-3 w-75" placeholder="Email" name="email" value={email}/>
+
+        <button type="submit" className="btn btn-primary">Crear</button>
+
+    </form>);
 };
